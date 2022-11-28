a28ea98539f084a239f369cb4a503920f51824ae
Jessie Wang <jessie.wang.daylil@gmail.com>
2022-11-28 22:31:54 +0800

    test1

--- www/0.project1/info.php
+++ www/0.project1/info.php
@@ -2,14 +2,15 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+123
 
 <body>
-<select></select>
+  <select></select>
 </body>
 
 </html>
